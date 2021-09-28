5830264a9a2f4d5dd8c883bb5e93c96f55688be1
owentlarkin <owenthomaslarkin@gmail.com>
2021-09-27 20:16:45 -0400

    Add project files.

---
+++ DYMOTest50/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace DYMOTest50
+{
+ partial class Form1
+ {
+  /// <summary>
+  ///  Required designer variable.
+  /// </summary>
+  private System.ComponentModel.IContainer components = null;
+
+  /// <summary>
+  ///  Clean up any resources being used.
+  /// </summary>
+  /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+  protected override void Dispose(bool disposing)
+  {
+   if (disposing && (components != null))
+   {
+    components.Dispose();
+   }
+   base.Dispose(disposing);
+  }
+
+  #region Windows Form Designer generated code
+
+  /// <summary>
+  ///  Required method for Designer support - do not modify
+  ///  the contents of this method with the code editor.
+  /// </summary>
+  private void InitializeComponent()
+  {
+   this.TB1 = new System.Windows.Forms.TextBox();
+   this.RTB1 = new System.Windows.Forms.RichTextBox();
+   this.SuspendLayout();
+   // 
+   // TB1
+   // 
+   this.TB1.Location = new System.Drawing.Point(12, 12);
+   this.TB1.Name = "TB1";
+   this.TB1.Size = new System.Drawing.Size(330, 23);
+   this.TB1.TabIndex = 0;
+   // 
+   // RTB1
+   // 
+   this.RTB1.Location = new System.Drawing.Point(12, 52);
+   this.RTB1.Name = "RTB1";
+   this.RTB1.Size = new System.Drawing.Size(740, 373);
+   this.RTB1.TabIndex = 1;
+   this.RTB1.Text = "";
+   // 
+   // Form1
+   // 
+   this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+   this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+   this.ClientSize = new System.Drawing.Size(800, 450);
+   this.Controls.Add(this.RTB1);
+   this.Controls.Add(this.TB1);
+   this.Name = "Form1";
+   this.Text = "DYMOTest50";
+   this.ResumeLayout(false);
+   this.PerformLayout();
+
+  }
+
+  #endregion
+
+  private System.Windows.Forms.TextBox TB1;
+  private System.Windows.Forms.RichTextBox RTB1;
+ }
+}
+
---
+++ DYMOTest50/Form1.cs
@@ -0,0 +1,61 @@
+﻿using DymoSDK.Implementations;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DYMOTest50
+{
+ public partial class Form1 : Form
+ {
+  public static string Format_exception(Exception e, string header = "Error")
+  {
+   var erm = new StringBuilder();
+   erm.Append(header);
+   erm.Append("\n");
+
+
+   if (e is AggregateException)
+   {
+    AggregateException ae = e as AggregateException;
+    erm.Append("One or more errors have occured in a background process." + "\n");
+    foreach (var e1 in ae.Flatten().InnerExceptions)
+    {
+     erm.Append("\n");
+     erm.Append(e1.Message);
+     if (e1.StackTrace is object)
+     {
+      erm.Append("\n" + "Stack Trace:" + "\n");
+      erm.Append(e1.StackTrace);
+     }
+    }
+   }
+   else
+   {
+    erm.Append(e.Message);
+    if (e.StackTrace is object)
+    {
+     erm.Append("\n" + "Stack Trace:" + "\n");
+     erm.Append(e.StackTrace);
+    }
+   }
+
+   return erm.ToString();
+  }
+
+  public Form1()
+  {
+   InitializeComponent();
+   try
+   {
+    DymoSDK.App.Init();
+    var Dsl = DymoLabel.Instance;
+    TB1.Text = Dsl.ToString();
+   }
+   catch (Exception Ex)
+   {
+    RTB1.Text = Format_exception(Ex);
+   }
+  }
+
+ }
+}
